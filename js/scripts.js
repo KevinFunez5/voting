71cd64d7af313eb2f189eb76d6185c83990f4940
Kevin Funez <kevin.funez315@gmail.com>
2021-08-12 10:40:34 -0700

    add age text
    Co-authored-by: Chynna Lew <chynnalew@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -12,17 +12,3 @@ $(document).ready(function(){
   });
 });
 
-// $(document).ready(function() {
-//   $("#age").submit(function(event) {
-//     event.preventDefault();
-//     const age = parseInt($("input#inputAge").val());
-
-//     if (age >= 18) {
-//       $("#ofAge").show();
-//       $("#tooYoung").hide();
-//     } else {
-//       $("#tooYoung").show();
-//       $("#ofAge").hide();
-//     }
-//   });
-// });
